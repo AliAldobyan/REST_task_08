1b2a639d69a58f96fbe20656a6a23982cf9daf9e
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-09-16 16:33:49 +0300

    REST_task_08 completed

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 from .models import Flight, Booking, Profile
 
 
@@ -11,15 +11,28 @@ class FlightSerializer(serializers.ModelSerializer):
 
 
 class BookingSerializer(serializers.ModelSerializer):
+	flight = serializers.SlugRelatedField(
+		many=False,
+		read_only = True,
+		slug_field = 'destination'
+	)
+	
 	class Meta:
 		model = Booking
 		fields = ['flight', 'date', 'id']
 
 
 class BookingDetailsSerializer(serializers.ModelSerializer):
+	total = serializers.SerializerMethodField()
+	flight = FlightSerializer()
+
 	class Meta:
 		model = Booking
-		fields = ['flight', 'date', 'passengers', 'id']
+		fields = ['flight', 'date', 'passengers', 'id', 'total']
+
+	def get_total(self, obj):
+		total = obj.flight.price * obj.passengers
+		return total
 
 
 class AdminUpdateBookingSerializer(serializers.ModelSerializer):
@@ -51,8 +64,32 @@ class RegisterSerializer(serializers.ModelSerializer):
         return validated_data
 
 
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['first_name', 'last_name']
+
+
 class ProfileSerializer(serializers.ModelSerializer):
+	user = UserSerializer()
+	past_bookings = serializers.SerializerMethodField()
+	tier = serializers.SerializerMethodField()
+
 	class Meta:
 		model = Profile
-		fields = ['user', 'miles']
+		fields = ['user', 'miles', 'past_bookings', 'tier']
 
+	def get_past_bookings(self, obj):
+		bookings_objs = obj.user.bookings.filter(date__lt=timezone.now())
+		bookings_json = BookingDetailsSerializer(bookings_objs, many=True).data
+		return bookings_json
+
+	def get_tier(self, obj):
+		if obj.miles < 10000:
+			return "Blue"
+		elif obj.miles < 60000:
+			return "Silver"
+		elif obj.miles < 100000:
+			return "Gold"
+		else:
+			return "Platinum"
